00774ed3c9e6d9c946eeabfe69e692dcbb125828
Stjefan <44638333+Stjefan@users.noreply.github.com>
2023-04-08 21:00:26 +0200

    ba

---
+++ src/components/FormsProps.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>My component</div>
+  {{ bla }}
+  <FormKit type="form" @submit="handleSubmit">
+    <FormKit type="text" :value="bla" label="PropVal" />
+  </FormKit>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+  // name: 'ComponentName'
+  props:
+  {
+    'bla': {
+      type: String
+    }
+  },
+  setup() {
+    function handleSubmit(args: any) {
+      console.log(args)
+
+    }
+    return {
+      handleSubmit
+    }
+  }
+})
+</script>
